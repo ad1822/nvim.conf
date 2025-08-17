39dac1006fdc99b0f7b9b3660c96a41ccf5691ad
Ayush <ayushdumasia00@gmail.com>
2025-08-17 16:56:55 +0530

    fix 🐛: Fix Cursor latency and animation

--- nvim/lua/plugins/ui/cursor.lua
+++ nvim/lua/plugins/ui/cursor.lua
@@ -10,8 +10,8 @@ return {
 		legacy_computing_symbols_support = false,
 
 		-- Smear Effect
-		smear_insert_mode = true,
-		stiffness = 1,
+		-- smear_insert_mode = true,
+		-- stiffness = 1,
 		trailing_stiffness = 0.5,
 		damping = 0.67,
 		matrix_pixel_threshold = 0.1,
